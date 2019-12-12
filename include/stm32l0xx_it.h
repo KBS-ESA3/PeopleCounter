23800c3e4e7d12061d83870ce90977685fca9149
Jossie-2000 <s1117117@student.windesheim.nl>
2019-12-12 10:17:15 +0100

    Deleted some unnecessary interrupt handlers

--- include/stm32l0xx_it.h
+++ include/stm32l0xx_it.h
@@ -1,10 +1,6 @@
 #ifndef __STM32L0xx_IT_H
 #define __STM32L0xx_IT_H
 
-void NMI_Handler(void);
-void HardFault_Handler(void);
-void SVC_Handler(void);
-void PendSV_Handler(void);
 void SysTick_Handler(void);
 void EXTI0_1_IRQHandler(void);
 void EXTI4_15_IRQHandler(void);
