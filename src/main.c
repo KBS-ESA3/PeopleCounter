662a61361c2291b27856b1694e3be4596bb683f4
hansleon <leon.hans@live.nl>
2020-01-09 15:50:07 +0100

    Test met TPPresence ipv TPObject

--- src/main.c
+++ src/main.c
@@ -22,6 +22,8 @@
 #define CALIPILE_INTTIM 0
 #define CALIPILE_OBJTRSHLD 0
 
+#define CALIPILE_TRSHLD 15
+
 calipile_t sensor0 = {
     CALIPILE_0_ADDR,
     CALIPILE_LOWPASS1,
@@ -59,8 +61,8 @@ int main(void)
     I2C_Init();
     calipile_init(&sensor0);
     calipile_init(&sensor1);
-    UART_clearScreen();
-    UART_PutStr("motion\n");
+    // UART_clearScreen();
+    // UART_PutStr("motion\n");
     // int16_t motion = 0;
     int16_t object = 0;
     int16_t objectOld = 0;
@@ -85,61 +87,80 @@ int main(void)
     {
         // int16_t motionOld = motion;
         objectOld = object;
-        object = calipile_getTPObject(&sensor0);
+        object = calipile_getTPPresence(&sensor0);
         objectDifOld = objectDif;
         objectDif = object - objectOld;
         objectDifDif = objectDif - objectDifOld;
 
         objectOld1 = object1;
-        object1 = calipile_getTPObject(&sensor1);
+        object1 = calipile_getTPPresence(&sensor1);
         objectDifOld1 = objectDif1;
         objectDif1 = object1 - objectOld1;
         objectDifDif1 = objectDif1 - objectDifOld1;
-        // motion = calipile_getTPMotion(&sensor0);
-        // UART_PutInt(motion - motionOld);
-        // UART_PutStr(" ");
-        // UART_PutInt(motion);
-        // UART_PutStr(" ");
-        // UART_PutInt(objectDifDif);
+
+        // if ((step == 0 || step == 1) && objectDif > CALIPILE_TRSHLD)
+        // {
+        //     if (objectDif > topPeak)
+        //     {
+        //         topPeak = objectDif;
+        //     }
+        //     step = 1;
+        // }
+        // if ((step == 1 || step == 2) && objectDif < -CALIPILE_TRSHLD)
+        // {
+        //     if (objectDif < botPeak)
+        //     {
+        //         botPeak = objectDif;
+        //     }
+        //     step = 2;
+        // }
+        // if (step == 2 && objectDif > -CALIPILE_TRSHLD)
+        // {
+        //     if (topPeak > (botPeak * -1))
+        //     {
+        //         people++;
+        //     }
+        //     else
+        //     {
+        //         people--;
+        //     }
+        //     step = 0;
+        //     topPeak = 0;
+        //     botPeak = 0;
+        // }
+
+        // UART_PutInt(people);
+        // UART_PutInt(object);
+        // UART_PutInt(objectDif);
         // UART_PutStr(" ");
         // UART_PutInt(objectDif1);
         // UART_PutStr(" ");
         // UART_PutInt(object);
         // UART_PutStr(" ");
-        // UART_PutInt(objectDif1 + 200);
-        // UART_PutStr(" ");
         // UART_PutInt(object1);
         // UART_PutStr(" ");
-        //UART_PutInt(object);
+        // UART_PutInt(objectDifDif);
+        // UART_PutInt(objectDif);
+
+        UART_PutInt(object1);
         UART_PutStr("\n");
 
-        if((step == 0 || step == 1) && objectDif > 20){
-            if(objectDif > topPeak){
-                topPeak = objectDif;
-            }
-            step = 1; 
-        }
-        if((step == 1 || step == 2) && objectDif < -20){
-            if(objectDif < botPeak){
-                botPeak = objectDif;
-            }
-            step = 2;
-        }
-        if(step == 2 && objectDif > -20){
-            if(topPeak > (botPeak * -1)){
-                people++;
-            }else{
-                people--;
-            }
-            step = 0;
-            topPeak = 0;
-            botPeak = 0;
-        }
-        
-        //UART_PutInt(people);
-        UART_PutInt(objectDif);
+        // if (objectDif1 > topPeak)
+        // {
+        //     topPeak = objectDif1;
+        //     UART_PutInt(topPeak);
+        //     UART_PutStr(" ");
+        //     UART_PutStr("\n");
+        // }
+        // if (objectDif1 < botPeak)
+        // {
+        //     botPeak = objectDif1;
+        //     UART_PutInt(botPeak);
+        //     UART_PutStr(" ");
+        //     UART_PutStr("\n");
+        // }
 
         HAL_GPIO_TogglePin(LED1_GPIO_PORT, LED1_PIN);
-        HAL_Delay(25);
+        HAL_Delay(50);
     }
 }
