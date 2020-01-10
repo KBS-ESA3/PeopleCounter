38fc3a3a558f1cfa88ebceae9fa2d04b69f3a98b
matthijs prent <matthijsprent@hotmail.com>
2020-01-10 12:16:27 +0100

    added defines file

---
+++ include/defines.h
@@ -0,0 +1,8 @@
+#ifndef __DEFINES_H
+#define __DEFINES_H
+
+// The ID from this device. Should be different for every 
+// Sensor that is conneted to the gateway. 0 - 127.
+#define DEVICE_ID 15
+
+#endif /* __DEFINES_H */
